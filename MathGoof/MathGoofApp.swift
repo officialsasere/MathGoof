d1c7ab1ea11c1a507859c002d5528e0834db9cd3
Emmanuel-Sasere <emmanuelscript.code@gmail.com>
2026-01-24 03:14:07 +0100

    Initial Commit

---
+++ MathGoof/MathGoofApp.swift
@@ -0,0 +1,17 @@
+//
+//  MathGoofApp.swift
+//  MathGoof
+//
+//  Created by Sasere on 24/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct MathGoofApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MathGoofTests/MathGoofTests.swift
@@ -0,0 +1,17 @@
+//
+//  MathGoofTests.swift
+//  MathGoofTests
+//
+//  Created by Sasere on 24/1/2026.
+//
+
+import Testing
+@testable import MathGoof
+
+struct MathGoofTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
